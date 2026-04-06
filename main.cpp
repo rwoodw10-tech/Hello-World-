bcab819d04fe7592e786551265469eab9dc1f95c
A Student <student@nowhere.edu>
2026-04-06 02:45:31 +0000

    Apr 5, 2026, 7:45 PM

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
   int teaspoons;
   int tablespoons;
   int cups;
-  
+  int quart;
   int remainingTeaspoons;
   cout << "Enter a measurement in teaspoons:" << endl;
   cin >> teaspoons;
