d2bc6a4dabdff7cce4d847b684fcb67b6d307df8
A Student <student@nowhere.edu>
2026-04-13 20:16:01 +0000

    Apr 13, 2026, 1:16 PM

--- main.cpp
+++ main.cpp
@@ -4,21 +4,5 @@ using namespace std;
 
 int main()
 {
-  int teaspoons;
-  int tablespoons;
-  int cups;
-  int quart;
-  int remainingTeaspoons;
-  cout << "Enter a measurement in teaspoons:" << endl;
-  cin >> teaspoons;
-  quart = teaspoons / 192;
-  remainingTeaspoons = teaspoons % 192;
-  cups = remainingTeaspoons / 48;
-  remainingTeaspoons = teaspoons % 48;
-  tablespoons = remainingTeaspoons / 3;
-  teaspoons = remainingTeaspoons % 3;
-  cout << quart << "quart(s)" << endl;
-  cout << cups << "cups(s)" << endl;
-  cout << tablespoons << "tablespoons(s)" << endl;
-  cout << teaspoons << "teaspoons(s)" << endl;
+  ;
 }
