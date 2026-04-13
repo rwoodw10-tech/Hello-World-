3ed4a1bf53c7c5cecaba09508cf55d17e7d1d029
A Student <student@nowhere.edu>
2026-04-13 20:49:25 +0000

    Apr 13, 2026, 1:49 PM

--- main.cpp
+++ main.cpp
@@ -16,5 +16,5 @@ int main()
   double verticalVelocity;
   verticalVelocity = velocityOfBike * sin(rampAngle);
   double timeBikeInAir;
-  timeBikeInAir = verticalVelocity 
+  timeBikeInAir = verticalVelocity *
 }
