d6a3e1d3c44aceee0ddaf595c528433bcc3dfdb1
A Student <student@nowhere.edu>
2026-04-15 23:03:30 +0000

    Apr 15, 2026, 4:03 PM

--- main.cpp
+++ main.cpp
@@ -11,12 +11,15 @@ int main()
   cin >> rampAngle;
   double velocityOfBike;
   cin >> velocityOfBike;
+  
   double horizontalVelocity;
   horizontalVelocity = velocityOfBike * cos(rampAngle);
+
   double verticalVelocity;
   verticalVelocity = velocityOfBike * sin(rampAngle);
   
   double accel_DueToGrav_;
+
   double timeBikeInAir;
   timeBikeInAir = verticalVelocity + sqrt(pow(verticalVelocity, 2) + (2 * 32.17405 )
 }
