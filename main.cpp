d0b2a2cce1f4da363fa149b7ea47c0a27b6bb111
A Student <student@nowhere.edu>
2026-04-16 20:13:57 +0000

    Apr 16, 2026, 1:13 PM

--- main.cpp
+++ main.cpp
@@ -49,6 +49,6 @@ int main() {
 
   cout << "Maximum Height: " << maxHeight << "feet" << endl;
 
-  cout << "Jump Distance: " << JumpDistance << 
+  cout << "Jump Distance: " << JumpDistance << "feet"
 
 }
