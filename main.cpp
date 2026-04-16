ece317338ee724af66d3b3dc00570afc5c1969b4
A Student <student@nowhere.edu>
2026-04-16 20:24:59 +0000

    Apr 16, 2026, 1:24 PM

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
   cout << "Enter Ramp Angle: ";
   cin >> rampAngle;
   double rampAngleRadian;
-  double Pi = numbers
+  double Pi = numbers::pi;
   rampAngleRadian = rampAngle * ()
 
   double velocityOfBike;
