13abaa0ff5c7862bd0aa7927c10021fd0af6b813
A Student <student@nowhere.edu>
2026-04-16 02:01:13 +0000

    Apr 15, 2026, 7:01 PM

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main() {
   double timeBikeInAir;
   timeBikeInAir = verticalVelocity + sqrt(VertVelocityPow2 + (2 * accel_DueToGrav_ * rampHeight));
   
-  //cout << heightOfRamp << " " << horizontalVelocity << endl;
+  //cout << r << " " << horizontalVelocity << endl;
   //cout << timeBikeInAir << endl;
   
   double maxHeight;
