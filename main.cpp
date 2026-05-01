16e278c23ca89f9fd0c6e011b20dc4d83889c3aa
A Student <student@nowhere.edu>
2026-05-01 20:07:56 +0000

    May 1, 2026, 1:07 PM

--- main.cpp
+++ main.cpp
@@ -4,56 +4,5 @@
 using namespace std;
 
 int main() {
-  double rampHeight;
-  cout << "Enter Ramp Height: ";
-  cin >> rampHeight;
-  
-  double rampAngle;
-  cout << "Enter Ramp Angle: ";
-  cin >> rampAngle;
-  double rampAngleRadian;
-  double PI = numbers::pi;
-  rampAngleRadian = rampAngle * (PI / 180);
-
-  double velocityOfBike;
-  cout << "Enter Velocity of the Bike: ";
-  cin >> velocityOfBike;
-
-  double horizontalVelocity;
-  horizontalVelocity = velocityOfBike * cos(rampAngleRadian);
-
-  double verticalVelocity;
-  verticalVelocity = velocityOfBike * sin(rampAngleRadian);
-  
-  double accel_DueToGrav_;
-  accel_DueToGrav_= 32.17405;
-  double VertVelocityPow2;
-  VertVelocityPow2 = pow(verticalVelocity, 2);
-  double timeBikeInAir_1;
-  timeBikeInAir_1 = verticalVelocity + sqrt(VertVelocityPow2 + (2 * accel_DueToGrav_ * rampHeight));
-  //cout << timeBikeInAir_1 << endl;
-  double timeBikeInAir_true;
-  timeBikeInAir_true = timeBikeInAir_1 / accel_DueToGrav_;
-  //cout << timeBikeInAir_2 << endl;
-  //cout << rampHeight << " " << horizontalVelocity << endl;
-  
-  double maxHeight;
-  maxHeight = rampHeight + (VertVelocityPow2 / (2 * accel_DueToGrav_));
-  //cout << maxHeight << endl;
-
-  double JumpDistance;
-  JumpDistance = horizontalVelocity * timeBikeInAir_true;
-  double landSpeed;
-  landSpeed = sqrt(pow(horizontalVelocity, 2) + (VertVelocityPow2 + (2 * accel_DueToGrav_ * rampHeight)));
-  
-  cout << "Horizontal Velocity: " << horizontalVelocity << " ft/s" << endl;
-
-  cout << "Air Time: " << timeBikeInAir_true << " seconds" << endl;
-
-  cout << "Maximum Height: " << maxHeight << " feet" << endl;
-
-  cout << "Jump Distance: " << JumpDistance << " feet" << endl;
-
-  cout << "Landing Speed: " << landSpeed << " ft/s" << endl;
-
+ 
 }
