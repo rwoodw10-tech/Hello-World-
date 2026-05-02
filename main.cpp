f9c0183fd746bd00182f11e1b4765244c9d2afac
A Student <student@nowhere.edu>
2026-05-02 01:21:14 +0000

    May 1, 2026, 6:21 PM

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main() {
   cout << "Int." << "\t";
   cout << "Pay" << "\t";
   cout << "Balance" << endl;
+  
   //cout << month << "\t";
   //cout << interestRate << "\t";
   //cout << amountPayed << "\t";
