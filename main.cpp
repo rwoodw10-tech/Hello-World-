90158ae10ecc6cc4e2f56aafe218ae03edfb5eae
A Student <student@nowhere.edu>
2026-05-02 01:19:24 +0000

    May 1, 2026, 6:19 PM

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
- cout << fixed << setprecision(2);
+ ?cout << fixed << setprecision(2);
  cout << "Month" << "\t";
  cout << "Int." << "\t";
  cout << "Pay" << "\t";
