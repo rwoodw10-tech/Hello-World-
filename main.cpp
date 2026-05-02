8a110379e83278476607fa6463d4409f0aa3cc11
A Student <student@nowhere.edu>
2026-05-02 02:03:42 +0000

    May 1, 2026, 7:03 PM

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main() {
  cin >> interestRate;
  double APR = interestRate / 12;
  cout << APR << endl;
- double calculation = (accountBalance + APR) - 150;
+ double calculation = (accountBalance ) - 150;
  cout << "Calculation: " << calculation << endl;
  for (month = 0; accountBalance <= 0; ++month){
   cout << "Month" << "\t";
