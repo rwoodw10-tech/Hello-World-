cdb3a26fa0aa767f61e158686335fdaba04a3f8f
A Student <student@nowhere.edu>
2026-05-05 02:02:29 +0000

    May 4, 2026, 7:02 PM

--- main.cpp
+++ main.cpp
@@ -5,41 +5,49 @@ using namespace std;
 
 int main() {
  cout << fixed << setprecision(2);
- /*cout << "Month" << "\t";
- cout << "Int." << "\t";
- cout << "Pay" << "\t";
- cout << "Balance" << endl;*/
- int month;
- double interestRate;
- double amountPayed;
- double accountBalance;
- cout << "Enter Account Balance: ";
- cin >> accountBalance;
- cout << "Enter Payment: ";
- cin >> amountPayed;
- cout << "Enter APR: ";
- cin >> interestRate;
- //double APR = (interestRate / 12) * 10;
- //cout << APR << endl;
- //double calculation = (accountBalance -150) + APR;
- //cout << "Calculation: " << calculation << endl;
- for (month = 0; accountBalance <= 0; ++month){
-  cout << "Month" << "\t";
-  cout << "Int." << "\t";
-  cout << "Pay" << "\t";
-  cout << "Balance" << endl;
-  if (month > 0) {
-    double APR = (interestRate / 12) * 10;
-    double calculation = (accountBalance -150) + APR;
-    cout << "Calculation: " << calculation << "\t" << APR << endl;
-  }
-  else if (accountBalance <= 150) {
-    accountBalance -= accountBalance;
-  }
-  //cout << month << "\t";
-  //cout << interestRate << "\t";
-  //cout << amountPayed << "\t";
-  //cout << accountBalance << "\t";
+int month = 12, payment = 0;
+    double interest = 0, monthlyInterest = 0, APR = 0, balance = 0;
 
- }
+    // std::cout << std::fixed << std::setprecision(2);
+    std::cout << "enter balance" << std::endl;
+    std::cin >> balance;
+    std::cout << "enter payment" << std::endl;
+    std::cin >> payment;
+    std::cout << "enter APR" << std::endl;
+    std::cin >> APR;
+
+    interest = (APR / 12) / 100;
+
+    // 19.9 / 12 = 1.65833 / 100 = 0.0165833 (interest)
+    // balance * interest = 16.5833 (monthlyInterest)
+    // calculation = (balance - payment) + monthlyInterest
+    std::cout << "interest calculated: " << interest << std::endl;
+
+    std::cout << "Month " << "Interest " << "Payment " << "Balance " << std::endl;
+
+    for (int i = 0; i < month; ++i)
+    {
+
+        if (i == 0)
+        {
+            std::cout << i << '\t';
+            std::cout << monthlyInterest << '\t';
+            std::cout << payment << '\t';
+            std::cout << balance << std::endl;
+        }
+        else
+        {
+            monthlyInterest = (balance * interest);
+            // std::cout << monthlyInterest << std::endl;
+
+            balance -= payment;
+            balance = balance + monthlyInterest;
+            std::cout << i << '\t';
+            std::cout << monthlyInterest << '\t';
+            std::cout << payment << '\t';
+            std::cout << balance << std::endl;
+        }
+    }
+
+    return 0;
 }
